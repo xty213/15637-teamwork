32d27a49451a31c34ae9c5d3ce08aba7796bc048
Haoliang Quan <hquan@andrew.cmu.edu>
2014-11-09 22:19:53 -0500

    add buyer view homepage

--- project/website/urls.py
+++ project/website/urls.py
@@ -7,9 +7,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^$', views.home),
+    url(r'^$', views.home),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html', 'extra_context':{'page_title':'Login'}}, name='login'),
-    #url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^confirm_registration/(?P<username>[a-zA-Z0-9_@\+\-]+)/(?P<token>[a-z0-9\-]+)$', views.confirm_registration, name='confirm_registration'),
+    url(r'^buyer_view/$', views.buyer_view, name='buyer_view'),
 )
