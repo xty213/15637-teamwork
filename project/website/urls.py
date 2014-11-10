b327122f5553f6c0f78adec3a4fd6e06125b501e
Haoliang Quan <hquan@andrew.cmu.edu>
2014-11-10 02:32:14 -0500

    add name for home url

--- project/website/urls.py
+++ project/website/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', views.home),
+    url(r'^$', views.home, name='home'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html', 'extra_context':{'page_title':'Login'}}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^register/$', views.register, name='register'),
