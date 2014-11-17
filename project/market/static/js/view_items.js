31a2ef5a02f45a2dbd8343e5c17089678993c1c0
Tianyu Xu <xty213@gmail.com>
2014-11-17 01:30:12 -0500

    implement question feature

--- project/market/static/js/view_items.js
+++ project/market/static/js/view_items.js
@@ -1,6 +1,7 @@
 function formatDate(date) {
     var hours = date.getHours();
     var minutes = date.getMinutes();
+    hours = hours < 10 ? '0'+hours : hours;
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes;
     return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + " " + strTime;
@@ -34,6 +35,42 @@ $(document).ready(function() {
                                 swal("It's a deal!", "Congratulations, this item is yours now!\nThe seller will got an email about the deal.", "success");
                                 $("#heading-btn-group").hide();
                                 $("<p class='pull-right text-danger'></p>").text("Sold at " + formatDate(new Date())).insertBefore("#heading-btn-group");
+                            }
+                        });
+            }
+        );
+        return false;
+    });
+
+    $("#form-ask-question").submit(function(e){
+        swal({
+            title: "Ask a question",
+            text: "Please write down your question here:",
+            type: "info",
+            showCancelButton: true,
+            confirmButtonText: 'Ok',
+            inputField: true,
+            },
+            function(question){
+                $("#form-ask-question input[name='question']").val(question);
+                $.post($("#form-ask-question").attr('action'), {
+                            'itemid':$("#form-ask-question input[name='itemid']").val(),
+                            "question":question,
+                            "csrfmiddlewaretoken":$("#form-ask-question input[name='csrfmiddlewaretoken']").val()
+                            },
+                        function(data){
+                            if(data != "success"){
+                                swal("Opps! An error?", "Something wrong with your request.\nThe error message is \"" + data + "\"\nPlease try again later.", "error");
+                            }
+                            else{
+                                swal("Good question!", "Wait for a sec, the answer is on the way.", "success");
+                                if ($("div.item-qa p").length == 0){
+                                    $("<li><span class='item-qa-sign'>Q:</span> <span>" + $("#form-ask-question input[name='question']").val() + "</span></li>").appendTo("div.item-qa ul");
+                                }
+                                else{
+                                    $("div.item-qa p").remove();
+                                    $("<ul><li><span class='item-qa-sign'>Q:</span> <span>" + $("#form-ask-question input[name='question']").val() + "</span></li></ul>").appendTo("div.item-qa");
+                                }
                             }
                         });
             }
