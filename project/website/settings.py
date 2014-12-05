d5e07870ababa7f671368e262a3a2152f2a15824
Ubuntu <ubuntu@ip-172-31-30-140.ec2.internal>
2014-12-05 05:29:43 +0000

    change database to mysql

--- project/website/settings.py
+++ project/website/settings.py
@@ -68,8 +68,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'webapp',
+	'USER': 'webapp',
+	'PASSWORD': 'password',
+	'HOST': '',
+	'PORT': '',
     }
 }
 
